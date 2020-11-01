89ce9d842a43093a11302d5e45ecde05de558f5f
anicschako <anicschacko@gmail.com>
2020-11-01 14:34:14 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs'); 
+const { measureMemory } = require('vm');
+const prefix = '>';
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () =>{
+    console.log('Our Bot is good to Go!');
+    client.user.set
+    client.user.setActivity("Helping Noobs!");
+})
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get(command).execute(message, args);
+    }
+})
+
+
+client.login('NzcyMjA2Mjk1MTcxMzM0MTY0.X53TJw.YPhdfuoQXq5HkI2_erBUVcY6OlA'); 
